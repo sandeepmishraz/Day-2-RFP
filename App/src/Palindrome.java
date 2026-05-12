7433134ec6c65efc605d82db7656b1508a230c3b
sandeep <sandeepmishra898k@gmail.com>
2026-05-12 17:07:02 +0530

    Palindrome number

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
 
-public class ReverseFor {
+public class Palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        int original = n;
         int rev = 0;
 
-        for (; n != 0; n /= 10) {
+        while (n != 0) {
             int digit = n % 10;
             rev = rev * 10 + digit;
+            n /= 10;
         }
 
-        System.out.println("Reverse = " + rev);
+        if (original == rev) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
